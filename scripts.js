a89b0440724253f6a40ead95a8c62cc6c07f85ee
IKLANLO <ilandaberea@yahoo.es>
2023-11-01 11:44:19 +0100

    versión final

--- scripts.js
+++ scripts.js
@@ -31,41 +31,59 @@ const militaryHelicopter = [
 ];
 
 class Gallery {
-    constructor(civilImages, militaryImages) {
-        this.civilImages = civilImages
-        this.militaryImages = militaryImages
-    }
+  constructor(civilImages, militaryImages) {
+    this.civilImages = civilImages
+    this.militaryImages = militaryImages
+  }
     
-    getRandomCivil = () => {
-        let randomImage = Math.floor(Math.random() * this.civilImages.length)
-        return this.civilImages[randomImage]
-    }
+  getRandomCivil = () => {
+    let randomImage = Math.floor(Math.random() * this.civilImages.length)
+    return this.civilImages[randomImage]
+  }
     
-    getRandomMilitary() {
-        let randomImage = Math.floor(Math.random() * this.militaryImages.length)
-        return this.militaryImages[randomImage]
-    }
+  getRandomMilitary = () => {
+    let randomImage = Math.floor(Math.random() * this.militaryImages.length)
+    return this.militaryImages[randomImage]
+  }
     
-    getAll() {
-        return this.civilImages.concat(this.militaryImages)
-    }
+  getAll = () => {
+    return this.civilImages.concat(this.militaryImages)
+  }
 }
 
 class Painter {
-    constructor() {
-    }
+  constructor() {
+    this.createGallery()
+  }
 
-    createGallery() {
-    }
+  createGallery = () => {
+    this.gallery = `<section></section>`
+    let appendSection = document.body.firstElementChild.innerHTML
+    appendSection += this.gallery
+    document.body.firstElementChild.innerHTML = appendSection
+  }
 
-    createImageTag(imageUrl) {
-    }
+  createImageTag = (imageUrl) => {
+    return `<picture>
+            <img src="${imageUrl}" />
+            </picture>`
+  }
 
-    paintSingleImage(imageUrl) {
-    }
+  paintSingleImage = (imageUrl) => {
+    document.body.firstElementChild.lastElementChild.innerHTML = ''
+    const imageTag = this.createImageTag(imageUrl)
+    let sectionElement = document.body.firstElementChild.lastElementChild
+    sectionElement.innerHTML += imageTag
+  }
 
-    paintMultipleImages(arrayOfImages) {
-    }
+  paintMultipleImages(arrayOfImages) {
+    document.body.firstElementChild.lastElementChild.innerHTML = ''
+    arrayOfImages.forEach((image) => {
+      const imageTag = this.createImageTag(image)
+      let sectionElement = document.body.firstElementChild.lastElementChild
+      sectionElement.innerHTML += imageTag
+    })
+  }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
